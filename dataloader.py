3e1d213fc19ebaf8b302905b855f5e8bd51d9154
yun-ss97 <yunseung_lee@korea.ac.kr>
2022-06-28 17:35:08 +0900

    Change dataloader

--- dataloader.py
+++ dataloader.py
@@ -1,70 +1,36 @@
 import os
-import pickle
 import torch
 from dataset import BuildDataset
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-
-import IPython
-
-def get_dataloader(args, window_size=60, slide_size=30, time_phase=False):
-    '''
-    Return data loader
-
-    Parameters:
-        data_root (str): path of the data
-        data_name (str): name of the data
-        batch_size (int): batch size
-        window_size (int): window size for time series condition
-        slide_size (int): moving window size
-        time_phase (bool): i.i.d. if False
-
-    Returns:
-        train/dev/tst dataloader (torch.utils.data.DataLoader):
-            output shape - time series: (batch size, window size, 31)
-                           i.i.d: (batch size, 31)
-    '''
-    with open(os.path.join(args.data_root, args.data_name), 'rb') as f:
-        df = pickle.load(f)
+from sklearn import preprocessing
 
 
-    if time_phase:
-        # time phase condition
-        print("time series condition")
-        trn = df[df['time_phase'] == 'trn']
-        dev = df[df['time_phase'] == 'val']
-        tst = df
-    else:
-        # i.i.d. condition
-        print("i.i.d. condition")
-        trn = df[df['iid_phase'] == 'trn']
-        dev = df[df['iid_phase'] == 'val']
-        tst = df
-        # IPython.embed();exit(1);
-        trn = pd.DataFrame(trn)
-        scaler = MinMaxScaler().fit(trn.iloc[:,:-3])
-        trn.iloc[:,:-3] = scaler.transform(trn.iloc[:,:-3])
-        # IPython.embed();exit(1);
-        dev.iloc[:,:-3] = scaler.transform(dev.iloc[:,:-3])
-        tst.iloc[:,:-3] = scaler.transform(tst.iloc[:,:-3])
+def get_dataloader(args, shuffle_dict):
+    df = pd.read_csv(args.processed_data)
+    df = df.set_index('TIME_IDX')
+    
+    trn = df[df['data']=='trn']
+    val = df[df['data']=='val']
+    tst = df[df['data']=='tst']
+    
+    min_max_scaler = preprocessing.MinMaxScaler()
+    trn.iloc[:, :-4] = min_max_scaler.fit_transform(trn.iloc[:, :-4].values)
+    val.iloc[:, :-4] = min_max_scaler.transform(val.iloc[:, :-4].values)
+    tst.iloc[:, :-4] = min_max_scaler.transform(tst.iloc[:, :-4].values)
 
-
-    print('train #:', trn.shape)
-    print('valid #:', dev.shape)
-    print('test #:', tst.shape)
-
-    trn_dataset = BuildDataset(trn, window_size, slide_size, time_phase)
-    dev_dataset = BuildDataset(dev, window_size, slide_size, time_phase)
-    tst_dataset = BuildDataset(tst, window_size, 1, time_phase, test=True)
+    trn_dataset = BuildDataset(args, trn, args.window_size, args.slide_size)
+    val_dataset = BuildDataset(args, val, args.window_size, args.slide_size)
+    tst_dataset = BuildDataset(args, tst, args.window_size, slide_size=1)
 
     trn_dataloader = torch.utils.data.DataLoader(
-        trn_dataset, batch_size=args.batch_size, shuffle=True, num_workers=8, drop_last=True
-    )
-    dev_dataloader = torch.utils.data.DataLoader(
-        dev_dataset, batch_size=args.batch_size, shuffle=False, num_workers=8, drop_last=True
+        trn_dataset, batch_size=args.batch_size, shuffle=shuffle_dict['trn'], num_workers=8, drop_last=False
+        )
+    val_dataloader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=args.batch_size, shuffle=shuffle_dict['val'], num_workers=8, drop_last=False
     )
     tst_dataloader = torch.utils.data.DataLoader(
-        tst_dataset, batch_size=args.batch_size, shuffle=False, num_workers=0, drop_last=True
+        tst_dataset, batch_size=args.batch_size, shuffle=shuffle_dict['tst'], num_workers=8, drop_last=False
     )
 
-    return trn_dataloader, dev_dataloader, tst_dataloader, scaler
+    return trn_dataloader, val_dataloader, tst_dataloader
+
--- dataset.py
+++ dataset.py
@@ -2,37 +2,24 @@ import torch
 from torch.utils.data import Dataset
 
 class BuildDataset(Dataset):
-    """
-    A class to build a custom dataset.
-
-    ...
-
-    Attributes
-    ----------
-    data: dataframe
-        input data
-    window_size: int
-        window size for time phase condition
-    slide_size: int
-        sliding size
-    time_phase: bool
-        i.i.d. condition if False
-
-    Methods
-    -------
-    __len__():
-        Return data size
-    __getitem__():
-        Return stacked tensor (input)
-    """
-    def __init__(self, data, window_size, slide_size, time_phase=True, test=False):
-        self.data = data.iloc[:, :-3].values
-        if test:
-            self.start_point = range(0, len(self.data)-window_size, slide_size)
-        else:
-            self.start_point = range(0, len(self.data), slide_size)
+    def __init__(self, args, data, window_size, slide_size, time_phase=True):
+        self.args = args
+        self.data = data
         self.window_size = window_size
+        self.slide_size = slide_size
         self.time_phase = time_phase
+        self.start_point = []
+        self.time_idx = []
+        period_start = 0
+        for i in range(self.args.total_period):
+            temp_df = self.data[(self.data['p_idx']==i)]
+            self.start_point.extend(range(period_start, period_start + len(temp_df)-self.window_size))
+            self.time_idx.extend(range(period_start + self.window_size - 1, period_start + len(temp_df)-1, self.slide_size))
+            period_start += len(temp_df)
+        
+        self.label = self.data.iloc[:,-3].values
+        self.data = self.data.iloc[:,:-4].values
+        self.time_idx = data.index.values[self.time_idx]
 
     def __len__(self):
         if self.time_phase:
@@ -42,7 +29,15 @@ class BuildDataset(Dataset):
 
     def __getitem__(self, idx):
         if self.time_phase:
-            return torch.FloatTensor(self.data[self.start_point[idx]:self.start_point[idx]+self.window_size])
+            output = torch.FloatTensor(self.data[self.start_point[idx]:self.start_point[idx]+self.window_size]) # (batch_size x input_size x seq_len)
+            if self.args.ae_type == 'mlp':
+                output = output.view(([self.args.num_var * self.args.window_size]))
+                
+            target = self.label[self.start_point[idx]:self.start_point[idx]+self.window_size]
+            if 1 in target:
+                target = 1
+            else:
+                target = 0
+            return torch.FloatTensor(output), target
         else:
             return torch.FloatTensor(self.data[idx])
-
